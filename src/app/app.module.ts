d467458088a5aeb9b03537de2c519ef08537441b
CaioVitor <caio.gobira@sunn-it.com>
2019-08-24 16:17:57 -0300

    Alterações na front-page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,36 @@
+import { MatCardModule } from '@angular/material/card';
 import { AppRoutingModule } from './app-routing.module';
 
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
 import { AppComponent } from './app.component';
 
-import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { FormsModule } from '@angular/forms';
 import { FileUploadComponent } from './file-upload/file-upload.component';
 import { Interceptor } from './interceptor.module';
 import { VideoPlayerComponent } from './video-player/video-player.component';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import { FooterComponent } from './footer/footer.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     FileUploadComponent,
-    VideoPlayerComponent
+    VideoPlayerComponent,
+    FooterComponent
   ],
   imports: [
     AppRoutingModule,
     BrowserModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    MDBBootstrapModule.forRoot(),
+    MatToolbarModule,
+    MatCardModule,
     FormsModule,
   ],
   providers: [
@@ -34,7 +40,7 @@ import { VideoPlayerComponent } from './video-player/video-player.component';
       multi: true
     },
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
     
